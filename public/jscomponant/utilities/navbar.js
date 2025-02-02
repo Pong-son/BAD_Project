3173b480b01bacaee8042cc4f6c88d8fb72e9775
Pong-son <lelouchui127@yahoo.com.hk>
2025-02-03 02:44:20 +0800

    navbar remove innerHTML

--- public/jscomponant/utilities/navbar.js
+++ public/jscomponant/utilities/navbar.js
@@ -1,75 +1,196 @@
 import { loginBtn } from './login.js'
 
-const navBar = (login) => {
-  let content = (login)?
-    `<nav class="navbar navbar-expand-lg text-bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand text-light" href="/">E & M</a>
-        <button id="navBarToggle" class="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active text-light" aria-current="page" href="/">Home</a>
-            </li>
-            <li class="nav-item dropdown" data-user>
-              <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Job
-              </a>
-              <ul class="dropdown-menu text-bg-primary">
-                <li><a class="dropdown-item text-light" href="/job_detail">Job Details</a></li>
-                <li><a class="dropdown-item text-light" href="/schedule">Schedule</a></li>
-              </ul>
-            </li>
-            <li class="nav-item dropdown" data-user>
-              <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Equipments
-              </a>
-              <ul class="dropdown-menu text-bg-primary">
-                <li><a class="dropdown-item text-light" href="/equipment">Equipment List</a></li>
-                <li><a class="dropdown-item text-light" href="/parameter">Parameter</a></li>
-                <li><a class="dropdown-item text-light" href="/history">History</a></li>
-              </ul>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-light" href="/client">Client</a>
-            </li>
-            <li class="nav-item admin_hide">
-              <a class="nav-link text-light" href="/account">Account</a>
-            </li>
-          </ul>
-          <div class="d-flex">
-            <span type="button" class="btn material-symbols-outlined fs-2 text-light" id="lightDarkBtn" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Change the Theme">
-              toggle_off
-            </span>
-            <a href="/"><button type="button" id="loginBtn" class="btn btn-success text-light" data-bs-toggle="modal" data-bs-target="#loginModal"></button></a>
-          </div>
-        </div>
-      </div>
-    </nav>` : `
-    <nav class="navbar navbar-expand-lg text-bg-primary">
-      <div class="container-fluid">
-        <a class="navbar-brand text-light" href="/">E & M</a>
-        <button id="navBarToggle" class="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active text-light" aria-current="page" href="/">Home</a>
-            </li>
-          </ul>
-          <div class="d-flex">
-            <span type="button" class="btn material-symbols-outlined fs-2 text-light" id="lightDarkBtn" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Change the Theme">
-              toggle_off
-            </span>
-            <button type="button" id="loginBtn" class="btn btn-success text-light" data-bs-toggle="modal" data-bs-target="#loginModal"></button>
-          </div>
-        </div>
-      </div>
-    </nav>`
-  document.querySelector('#navBar').innerHTML = content
+const navBar = async (login) => {
+  let navTab = document.createElement('nav')
+  navTab.classList.add('navbar')
+  navTab.classList.add('navbar-expand-lg')
+  navTab.classList.add('text-bg-primary')
+
+  let divTab = document.createElement('div')
+  divTab.classList.add('container-fluid')
+
+  let aTab = document.createElement('a')
+  aTab.classList.add('navbar-brand')
+  aTab.classList.add('text-light')
+  aTab.setAttribute('href','/')
+  aTab.textContent = 'E & M'
+
+  let navDivTab = document.createElement('div')
+  navDivTab.classList.add('collapse')
+  navDivTab.classList.add('navbar-collapse')
+  navDivTab.setAttribute('id','navbarSupportedContent')
+
+  let ulTab = document.createElement('ul')
+  ulTab.classList.add('navbar-nav')
+  ulTab.classList.add('me-auto')
+  ulTab.classList.add('mb-2')
+  ulTab.classList.add('mb-lg-0')
+
+  let btnDivTab = document.createElement('div')
+  btnDivTab.classList.add('d-flex')
+
+  let btnATab = document.createElement('a')
+  btnATab.setAttribute('href', '/')
+
+  let btnTab = document.createElement('button')
+  btnTab.setAttribute('type', 'button')
+  btnTab.setAttribute('id', 'loginBtn')
+  btnTab.setAttribute('data-bs-toggle', 'modal')
+  btnTab.setAttribute('data-bs-target', '#loginModal')
+  btnTab.classList.add('btn')
+  btnTab.classList.add('btn-success')
+  btnTab.classList.add('text-light')
+
+  let firstLiTab = document.createElement('li')
+  firstLiTab.classList.add('nav-item')
+
+  let firstATab = document.createElement('a')
+  firstATab.classList.add('nav-link')
+  firstATab.classList.add('active')
+  firstATab.classList.add('text-light')
+  firstATab.setAttribute('aria-current','page')
+  firstATab.setAttribute('href', '/')
+  firstATab.textContent = 'Home'
+
+  let secondLiTab = document.createElement('li')
+  secondLiTab.classList.add('nav-item')
+  secondLiTab.classList.add('dropdown')
+  secondLiTab.setAttribute('data-user','')
+
+  let secondATab = document.createElement('a')
+  secondATab.classList.add('nav-link')
+  secondATab.classList.add('dropdown-toggle')
+  secondATab.classList.add('text-light')
+  secondATab.setAttribute('href', '/job')
+  secondATab.setAttribute('role', 'button')
+  secondATab.setAttribute('data-bs-toggle', 'dropdown')
+  secondATab.setAttribute('aria-expanded', 'false')
+  secondATab.textContent = 'Job'
+
+  let secondUlTab = document.createElement('ul')
+  secondUlTab.classList.add('dropdown-menu')
+  secondUlTab.classList.add('text-bg-primary')
+
+  let jobLiTab = document.createElement('li')
+
+  let jobATab = document.createElement('a')
+  jobATab.classList.add('dropdown-item')
+  jobATab.classList.add('text-light')
+  jobATab.setAttribute('href', '/job_detail')
+  jobATab.textContent = 'Job Details'
+
+  let scheduleLiTab = document.createElement('li')
+
+  let scheduleATab = document.createElement('a')
+  scheduleATab.classList.add('dropdown-item')
+  scheduleATab.classList.add('text-light')
+  scheduleATab.setAttribute('href', '/schedule')
+  scheduleATab.textContent = 'Schedule'
+
+  let thirdLiTab = document.createElement('li')
+  thirdLiTab.classList.add('nav-item')
+  thirdLiTab.classList.add('dropdown')
+  thirdLiTab.setAttribute('data-user','')
+
+  let thirdATab = document.createElement('a')
+  thirdATab.classList.add('nav-link')
+  thirdATab.classList.add('dropdown-toggle')
+  thirdATab.classList.add('text-light')
+  thirdATab.setAttribute('href', '/#')
+  thirdATab.setAttribute('role', 'button')
+  thirdATab.setAttribute('data-bs-toggle', 'dropdown')
+  thirdATab.setAttribute('aria-expanded', 'false')
+  thirdATab.textContent = 'Equipments'
+
+  let thirdUlTab = document.createElement('ul')
+  thirdUlTab.classList.add('dropdown-menu')
+  thirdUlTab.classList.add('text-bg-primary')
+
+  let equipmentLiTab = document.createElement('li')
+
+  let equipmentATab = document.createElement('a')
+  equipmentATab.classList.add('dropdown-item')
+  equipmentATab.classList.add('text-light')
+  equipmentATab.setAttribute('href', '/equipment')
+  equipmentATab.textContent = 'Equipment List'
+
+  let parameterLiTab = document.createElement('li')
+
+  let parameterATab = document.createElement('a')
+  parameterATab.classList.add('dropdown-item')
+  parameterATab.classList.add('text-light')
+  parameterATab.setAttribute('href', '/parameter')
+  parameterATab.textContent = 'Parameter'
+
+  let historyLiTab = document.createElement('li')
+
+  let historyATab = document.createElement('a')
+  historyATab.classList.add('dropdown-item')
+  historyATab.classList.add('text-light')
+  historyATab.setAttribute('href', '/history')
+  historyATab.textContent = 'History'
+
+  let forthLiTab = document.createElement('li')
+  forthLiTab.classList.add('nav-item')
+
+  let forthATab = document.createElement('a')
+  forthATab.classList.add('nav-link')
+  forthATab.classList.add('text-light')
+  forthATab.setAttribute('href', '/client')
+  forthATab.textContent = 'Client'
+
+  let fifthLiTab = document.createElement('li')
+  fifthLiTab.classList.add('nav-item')
+  fifthLiTab.classList.add('admin_hide')
+
+  let fifthATab = document.createElement('a')
+  fifthATab.classList.add('nav-link')
+  fifthATab.classList.add('text-light')
+  fifthATab.setAttribute('href', '/account')
+  fifthATab.textContent = 'Account'
+
+  if(login) {
+    firstLiTab.appendChild(firstATab)
+    jobLiTab.appendChild(jobATab)
+    secondUlTab.appendChild(jobLiTab)
+    scheduleLiTab.appendChild(scheduleATab)
+    secondUlTab.appendChild(scheduleLiTab)
+    secondLiTab.appendChild(secondATab)
+    secondLiTab.appendChild(secondUlTab)
+    equipmentLiTab.appendChild(equipmentATab)
+    thirdUlTab.appendChild(equipmentLiTab)
+    parameterLiTab.appendChild(parameterATab)
+    thirdUlTab.appendChild(parameterLiTab)
+    historyLiTab.appendChild(historyATab)
+    thirdUlTab.appendChild(historyLiTab)
+    thirdLiTab.appendChild(thirdATab)
+    thirdLiTab.appendChild(thirdUlTab)
+    forthLiTab.appendChild(forthATab)
+    fifthLiTab.appendChild(fifthATab)
+    ulTab.appendChild(firstLiTab)
+    ulTab.appendChild(secondLiTab)
+    ulTab.appendChild(thirdLiTab)
+    ulTab.appendChild(forthLiTab)
+    ulTab.appendChild(fifthLiTab)
+    btnATab.appendChild(btnTab)
+    btnDivTab.appendChild(btnATab)
+    navDivTab.appendChild(ulTab)
+    navDivTab.appendChild(btnDivTab)
+    divTab.appendChild(aTab)
+    divTab.appendChild(navDivTab)
+    navTab.appendChild(divTab)
+    document.querySelector('#navBar').appendChild(navTab)
+  } else {
+    firstLiTab.appendChild(firstATab)
+    ulTab.appendChild(firstLiTab)
+    btnDivTab.appendChild(btnTab)
+    navDivTab.appendChild(ulTab)
+    navDivTab.appendChild(btnDivTab)
+    divTab.appendChild(aTab)
+    divTab.appendChild(navDivTab)
+    navTab.appendChild(divTab)
+    document.querySelector('#navBar').appendChild(navTab)
+  }
 
   loginBtn()
 }
